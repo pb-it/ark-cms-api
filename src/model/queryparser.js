406318f7c643c39a5bcfd6d3c306fb16761ce7a1
Patrick Bauer <patrick.bauer@pb-it.at>
2023-03-01 07:56:55 +0100

    Refactoring and additional tests

---
+++ src/model/queryparser.js
@@ -0,0 +1,225 @@
+const inflection = require('inflection');
+
+class QueryParser {
+
+    _model;
+    _book;
+    _joins;
+
+    constructor(model, book) {
+        this._model = model;
+        this._book = book; //model.getBook();
+        this._joins = [];
+    }
+
+    query(prop, value) {
+        if (prop == 'or') {
+            var fn;
+            this._book = this._book.query(function (qb) {
+                if (Array.isArray(value)) { //TODO: parathesed queries fail currently because of missing joins
+                    qb.where(function (oqb) {
+                        var fn;
+                        for (var q of value) {
+                            for (const [p, v] of Object.entries(q)) {
+                                fn = this._query(p, v);
+                                //fn(oqb);
+                                oqb.orWhere(fn.bind(this));
+                            }
+                        }
+                    }.bind(this));
+                } else {
+                    for (const [p, v] of Object.entries(value)) {
+                        qb.orWhere(this._query(p, v));
+                    }
+                }
+            }.bind(this));
+        } else {
+            var fn = this._query(prop, value);
+            this._book = this._book.query(function (qb) {
+                fn(qb);
+            }.bind(this));
+        }
+    }
+
+    finalizeQuery() {
+        if (this._joins.length > 0) {
+            var tableName = this._model._tableName;
+            var id = inflection.singularize(tableName) + "_id";
+            for (let a of this._joins) {
+                this._book = this._book.query(function (qb) {
+                    qb.leftJoin(a, tableName + '.id', id);
+                    //console.log(qb.toSQL());
+                });
+            }
+        }
+        return this._book;
+    }
+
+    _query(prop, value) {
+        var fn;
+        var index = prop.indexOf('.');
+        if (index == -1) {
+            var propName;
+            var operator;
+
+            index = prop.lastIndexOf('_');
+            if (index == -1)
+                propName = prop;
+            else {
+                propName = prop.substring(0, index);
+                operator = prop.substring(index + 1);
+            }
+
+            var relAttr;
+            for (var attribute of this._model.getDefinition().attributes) {
+                if (attribute['dataType'] == "relation" && attribute['name'] == propName) {
+                    relAttr = attribute;
+                    break;
+                }
+            }
+            if (relAttr && relAttr['multiple']) {
+                var via = relAttr['via'];
+                if (via) {
+                    throw new Error("Not Implemented Yet");
+                } else {
+                    var val;
+                    if (operator == 'null')
+                        val = value;
+                    else {
+                        if (Array.isArray(value))
+                            val = value;
+                        else if (typeof value === 'string' || value instanceof String)
+                            val = value.split(',').map(Number);
+                    }
+                    if (!operator)
+                        operator = 'containsAny';
+                    fn = this._queryRelation(relAttr, operator, val);
+                }
+            } else {
+                if (operator)
+                    fn = this._queryComparisonOperation(propName, operator, value);
+                else {
+                    fn = function (qb) {
+                        if (Array.isArray(value))
+                            qb.where(propName, 'in', value);
+                        else
+                            qb.where(propName, value);
+                    };
+                }
+            }
+        }
+        return fn;
+    }
+
+    _queryRelation(relAttr, operation, value) {
+        var fn;
+        var subType = relAttr['model'];
+        var relModel = this._model.getShelf().getModel(subType);
+        if (relModel) {
+            var tableName = this._model._tableName;
+            var relModelTable = relModel.getTableName();
+            var junctionTable = this._model.getJunctionTableName(relAttr);
+            var id = inflection.singularize(tableName) + "_id";
+            var fid = inflection.singularize(relModelTable) + "_id";
+
+            if (this._joins.indexOf(junctionTable) == -1)
+                this._joins.push(junctionTable);
+
+            fn = function (qb) {
+                switch (operation) {
+                    case 'null':
+                        if (value === 'true')
+                            qb.where(fid, 'is', null);
+                        else
+                            qb.where(fid, 'is not', null);
+                        break;
+                    case 'containsAny': // includesSome
+                        if (Array.isArray(value))
+                            qb.whereIn(fid, value);
+                        else
+                            qb.where(fid, value);
+                        break;
+                    case 'ncontainsAny':
+                        if (Array.isArray(value))
+                            qb.whereRaw(tableName + '.id NOT IN (SELECT ' + id + ' FROM ' + junctionTable + ' WHERE ' + fid + ' IN (?) )', value);
+                        else
+                            qb.where(fid, 'is not', value);
+                        break;
+                    case 'containsAll': // includesEvery
+                        if (Array.isArray(value)) {
+                            qb.whereIn(fid, value)
+                                .groupBy(id)
+                                .havingRaw('COUNT(*) >= ?', value.length);
+                        } else
+                            qb.where(fid, value);
+                        break;
+                }
+            };
+        } else
+            throw new Error(`unkown type: ${subType}`);
+        return fn;
+    }
+
+    _queryComparisonOperation(propName, operator, value) {
+        return function (qb) {
+            switch (operator) {
+                case 'null':
+                    if (value === 'true')
+                        qb.where(propName, 'is', null); // whereNotNull
+                    else
+                        qb.where(propName, 'is not', null);
+                    break;
+                case 'in':
+                    throw new Error("Not Implemented Yet");
+                case 'nin':
+                    throw new Error("Not Implemented Yet");
+                case 'contains':
+                    qb.where(propName, 'is not', null).where(propName, 'like', `%${value}%`);
+                    break;
+                case 'ncontains':
+                    qb.where(propName, 'is', null).orWhere(propName, 'not like', `%${value}%`);
+                    break;
+                case 'eq':
+                    qb.where(propName, 'is not', null);
+                    if (Array.isArray(value)) {
+                        if (value.length > 0) {
+                            qb.where(function () {
+                                var bFirst = true;
+                                for (var val of value) {
+                                    if (bFirst) {
+                                        qb.where(propName, 'like', val);
+                                        bFirst = false;
+                                    } else
+                                        qb.orWhere(propName, 'like', val);
+                                }
+                            });
+                        }
+                    } else
+                        qb.where(propName, 'like', value);
+                    break;
+                case 'neq':
+                    if (Array.isArray(value))
+                        qb.where(propName, 'is', null).orWhere(propName, 'not in', value); // <> / !=
+                    else
+                        qb.where(propName, 'is', null).orWhere(propName, 'not like', value); // <> / !=
+                    break;
+                case 'lt':
+                    qb.where(propName, '<', value);
+                    break;
+                case 'gt':
+                    qb.where(propName, '>', value);
+                    break;
+                case 'lte':
+                    qb.where(propName, '<=', value);
+                    break;
+                case 'gte':
+                    qb.where(propName, '>=', value);
+                    break;
+                default:
+                    qb.where(prop, value);
+            }
+        }
+    }
+}
+
+module.exports = QueryParser;
---
+++ tests/search.test.js
@@ -0,0 +1,176 @@
+const fs = require('fs');
+
+if (!global.controller)
+    global.controller = require('../src/controller/controller');
+const WebClient = require('../src/common/webclient.js');
+const controller = require('../src/controller/controller');
+
+const ApiHelper = require('./helper/api-helper.js');
+const DatabaseHelper = require('./helper/database-helper');
+
+var apiUrl;
+var apiHelper;
+var databaseHelper;
+var shelf;
+var webclient;
+const bCleanupBeforeTests = false;
+const bCleanupAfterTests = true;
+
+beforeAll(async () => {
+    if (!controller.isRunning()) {
+        const server = require('./config/server-config');
+        const database = require('./config/database-config');
+        await controller.setup(server, database);
+        shelf = controller.getShelf();
+    }
+
+    webclient = new WebClient();
+
+    apiUrl = "http://localhost:" + controller.getServerConfig()['port'] + "/api"
+    apiHelper = new ApiHelper(apiUrl, webclient);
+    databaseHelper = new DatabaseHelper(shelf);
+
+    if (bCleanupBeforeTests)
+        ; //TODO:
+
+    return Promise.resolve();
+});
+
+afterAll(async () => {
+    if (bCleanupAfterTests) {
+        try {
+            var models = await apiHelper.getAllModels();
+            for (var model of models)
+                await databaseHelper.deleteModel(model);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    try {
+        await controller.teardown();
+    } catch (error) {
+        console.log(error);
+    }
+    return Promise.resolve();
+});
+
+test('movie_db', async function () {
+    var modelMovies = JSON.parse(fs.readFileSync('./tests/data/models/movies.json', 'utf8'));
+    await apiHelper.uploadModel(modelMovies);
+
+    var modelStudios = JSON.parse(fs.readFileSync('./tests/data/models/studios.json', 'utf8'));
+    await apiHelper.uploadModel(modelStudios);
+
+    var modelStars = JSON.parse(fs.readFileSync('./tests/data/models/stars.json', 'utf8'));
+    await apiHelper.uploadModel(modelStars);
+
+    await shelf.loadAllModels();
+    await shelf.initAllModels();
+
+    var data = await apiHelper.getAllModels();
+
+    var res = data.filter(function (x) {
+        return x['definition']['name'] === "movies";
+    })[0];
+    var modelMoviesId = res['id'];
+    expect(res['definition']).toEqual(modelMovies);
+
+    res = data.filter(function (x) {
+        return x['definition']['name'] === "studios";
+    })[0];
+    var modelStudiosId = res['id'];
+    expect(res['definition']).toEqual(modelStudios);
+
+    res = data.filter(function (x) {
+        return x['definition']['name'] === "stars";
+    })[0];
+    var modelStarsId = res['id'];
+    expect(res['definition']).toEqual(modelStars);
+
+    //insert testdata
+    var movies = JSON.parse(fs.readFileSync('./tests/data/crud/movies_2.json', 'utf8'));
+
+    var urlMovies = apiUrl + "/movies";
+    for (var movie of movies) {
+        await webclient.post(urlMovies, movie);
+    }
+
+    data = await webclient.curl(urlMovies);
+    expect(data.length).toEqual(movies.length);
+
+    var stars = JSON.parse(fs.readFileSync('./tests/data/crud/stars_2.json', 'utf8'));
+
+    var urlStars = apiUrl + "/stars";
+    for (var star of stars) {
+        await webclient.post(urlStars, star);
+    }
+
+    data = await webclient.curl(urlStars);
+    expect(data.length).toEqual(stars.length);
+
+    //search
+    var urlSearch;
+    var idArr;
+
+    urlSearch = apiUrl + "/stars?name_eq=Johnny Depp";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('3');
+
+    urlSearch = apiUrl + "/stars?name_contains=Chris";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('2');
+
+    urlSearch = apiUrl + "/stars?movies_null=true";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('5');
+
+    urlSearch = apiUrl + "/stars?movies_containsAny=1,3";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('1,2');
+
+    urlSearch = apiUrl + "/stars?movies_containsAll=1,3";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('1');
+
+    urlSearch = apiUrl + "/stars?movies_ncontainsAny=3";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('3,4,5,6,7');
+
+    urlSearch = apiUrl + "/stars?[or][movies]=3&[or][movies_null]=true";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('1,2,5');
+
+    /*urlSearch = apiUrl + "/stars?[or][0][movies]=3&[or][1][movies_null]=true";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('1,2,5');*/
+
+    urlSearch = apiUrl + "/stars?gender=female&movies_null=true";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('');
+
+    urlSearch = apiUrl + "/stars?gender=female&movies_null=true&[or][movies]=5";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('4,7');
+
+    urlSearch = apiUrl + "/stars?gender=female&[or][movies_null]=true&[or][movies]=5";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('4,5,6,7');
+
+    /*urlSearch = apiUrl + "/stars?gender=female&[or][0][movies_null]=true&[or][1][movies]=5";
+    data = await webclient.curl(urlSearch);
+    idArr = data.map(function (x) { return x['id'] });
+    expect(idArr.sort().join(',')).toEqual('7');*/
+
+    return Promise.resolve();
+});
